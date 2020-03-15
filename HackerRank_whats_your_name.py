d49afe49217bdcd980fae140277ac8c4d2b344e4
kulvirvirk <42256045+kulvirvirk@users.noreply.github.com>
2020-03-14 23:48:26 -0700

    Create HackerRank_whats_your_name.py

---
+++ HackerRank_whats_your_name.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/whats-your-name/problem 
+def print_full_name(a, b):
+  print("Hello " + a + " " + b + "! You just delved into python.")
+
+a = input('Enter first name: ')
+b = input('Enter last name: ')
+
+print_full_name(a,b)
